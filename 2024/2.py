f6346573b1806d882a32d820f1c3e5748dd21e97
Emil Murman <emil.murman@vermiculus.se>
2024-12-02 17:44:26 +0100

    solve day 2

--- 2024/2.py
+++ 2024/2.py
@@ -2,21 +2,53 @@
 
 from io import TextIOWrapper
 
-useSample = True
+useSample = False
+
+def solve(report):
+    if len(report) == 1:
+        result += 1
+    
+    isAscending = report[0] < report[1]
+    isSafe = True
+    for level in range(1, len(report)):
+        diff = report[level] - report[level - 1]
+        if abs(diff) > 3 or diff == 0:
+            isSafe = False
+            break
+            
+        if (diff > 0 and not isAscending) or (diff < 0 and isAscending):
+            isSafe = False
+            break
+        
+    return 1 if isSafe else 0
 
 def part1(f: TextIOWrapper):
     result = 0
+    reports = []
 
     for line in f.readlines():
-        pass
-    
+        reports.append([int(num) for num in line.split()])
+
+    for report in reports:
+        result += solve(report)
+
     print(f"Result: {result}")
 
 def part2(f: TextIOWrapper):
     result = 0
+    reports = []
 
     for line in f.readlines():
-        pass
+        reports.append([int(num) for num in line.split()])
+
+    for report in reports:
+        if len(report) == 1:
+            result += 1
+        
+        for i in range(len(report)):
+            if solve(report[:i] + report[i + 1:]):
+                result += 1
+                break
 
     print(f"Result: {result}")
 
