c3fa53a2b03fb6c04dfc7d94587d3c0f97315d95
Emil Murman <emil.murman@vermiculus.se>
2024-12-06 14:48:15 +0100

    brute force day 6

--- 2024/6.py
+++ 2024/6.py
@@ -2,23 +2,85 @@
 
 from io import TextIOWrapper
 
-useSample = True
+useSample = False
 
 def parse(f: TextIOWrapper):
-    result = None
+    grid = []
+    startingPos = None
 
-    for line in f.readlines():
-        pass
+    for i, line in enumerate(f.readlines()):
+        grid.append([])
+        for j, cell in enumerate(line.strip()):
+            if cell == "^":
+                startingPos = i, j
+                grid[-1].append(".")
+            else:
+                grid[-1].append(cell)
 
-    return result
+    return grid, startingPos
+
+def turnRight(direction):
+    di = 0 if direction[1] == 0 else direction[1]
+    dj = 0 if direction[0] == 0 else -direction[0]
+    return di, dj
 
 def part1(parsedInput):
     result = 0
+    grid, (i, j) = parsedInput
+    direction = (-1, 0)
+    visited = set()
+    visited.add((i, j))
 
+    while True:
+        nextI, nextJ = i + direction[0], j + direction[1]
+        if nextI < 0 or nextI >= len(grid) or nextJ < 0 or nextJ >= len(grid[0]):
+            break
+
+        if grid[nextI][nextJ] == "#":
+            direction = turnRight(direction)
+            continue
+
+        i, j = nextI, nextJ
+        visited.add((i, j))
+    
+    result = len(visited)
     print(f"Result: {result}")
 
 def part2(parsedInput):
     result = 0
+    grid, (i, j) = parsedInput
+    direction = (-1, 0)
+    visited = set()
+    visited.add((i, j) + direction)
+
+    for k in range(len(grid)):
+        print(f"Row {k + 1} / {len(grid)}")
+
+        for l in range(len(grid[0])):
+            if grid[k][l] == "#" or (k, l) == parsedInput[1]: continue
+            grid[k][l] = "0"
+
+            i, j = parsedInput[1]
+            direction = (-1, 0)
+            visited = set()
+            visited.add((i, j) + direction)
+
+            while True:
+                nextI, nextJ = i + direction[0], j + direction[1]
+                if nextI < 0 or nextI >= len(grid) or nextJ < 0 or nextJ >= len(grid[0]):
+                    break
+
+                if (nextI, nextJ) + direction in visited:
+                    result += 1
+                    break
+
+                if grid[nextI][nextJ] == "#" or grid[nextI][nextJ] == "0":
+                    direction = turnRight(direction)
+                    continue
+
+                i, j = nextI, nextJ
+                visited.add((i, j) + direction)
+            grid[k][l] = "."
 
     print(f"Result: {result}")
 
