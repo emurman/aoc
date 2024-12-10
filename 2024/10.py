1b06835a3647671653e0494532d4cd4a65fb7a65
Emil Murman <emil.murman@vermiculus.se>
2024-12-10 09:53:26 +0100

    classic DP for day 10

--- 2024/10.py
+++ 2024/10.py
@@ -1,24 +1,80 @@
 #!/usr/bin/python3
 
+from collections import deque
 from io import TextIOWrapper
 
-useSample = True
+useSample = False
 
 def parse(f: TextIOWrapper):
-    result = None
+    result = []
 
     for line in f.readlines():
-        pass
+        result.append([int(num) for num in line.strip()])
 
     return result
 
+directions = [
+    (0, 1),
+    (1, 0),
+    (0, -1),
+    (-1, 0)
+]
+
 def part1(parsedInput):
     result = 0
+    grid = parsedInput
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] != 0: continue
+            visited = set()
+            queue = deque()
+            queue.append((i, j))
+
+            while queue:
+                elem = queue.popleft()
+                if elem in visited: continue
+                visited.add(elem)
+
+                value = grid[elem[0]][elem[1]]
+                if value == 9: 
+                    result += 1
+                    continue
+
+
+                for di, dj in directions:
+                    newI, newJ = elem[0] + di, elem[1] + dj
+                    if newI < 0 or newI >= len(grid) or newJ < 0 or newJ >= len(grid[i]): continue
+
+                    if grid[newI][newJ] == value + 1:
+                        queue.append((newI, newJ))
 
     print(f"Result: {result}")
 
 def part2(parsedInput):
     result = 0
+    grid = parsedInput
+    dp_cache = {}
+
+    def dp(i, j):
+        if grid[i][j] == 0: return 1
+        if (i, j) in dp_cache: return dp_cache[(i, j)]
+
+        result = 0
+        for di, dj in directions:
+            newI, newJ = i + di, j + dj
+            if newI < 0 or newI >= len(grid) or newJ < 0 or newJ >= len(grid[i]): continue
+
+            if grid[newI][newJ] == grid[i][j] - 1:
+                result += dp(newI, newJ)
+
+        dp_cache[(i, j)] = result
+        return result
+    
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 9:
+                result += dp(i, j)
 
     print(f"Result: {result}")
 
